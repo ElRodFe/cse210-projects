7005da83a4303fe4b916995314aaf6f260b0b548
ElRodFe <el.rod.fe@gmail.com>
2023-09-21 11:09:07 -0300

    Prep 1 and 2 finished

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade porcentage?: ");
+        string gradePorcentage = Console.ReadLine();
+
+        int porcentage = int.Parse(gradePorcentage);
+
+        if (porcentage >= 90)
+        {   
+            Console.WriteLine("You have an 'A'");
+        }
+
+        else if (porcentage >= 80)
+        {
+            Console.WriteLine("You have a 'B'");
+        }
+
+        else if (porcentage >= 70)
+        {
+            Console.WriteLine("You have a 'C'");
+        }
+
+        else if (porcentage >= 60)
+        {
+            Console.WriteLine("You have a 'D'");
+        }
+
+        else
+        {
+            Console.WriteLine("You have a 'F'");
+        }
+
+
     }
 }
