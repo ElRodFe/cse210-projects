ce03ba5df7b1f4f28f6c7ba3ad04730259051483
ElRodFe <el.rod.fe@gmail.com>
2023-09-28 11:30:03 -0300

    Week 3 activity finished

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Google";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2010;
+        job1._endYear = 2012;
+        job1.Display();
+
+        Job job2 = new Job();
+        job2._company = "Microsoft";
+        job2._jobTitle = "Senior Developer";
+        job2._startYear = 2015;
+        job2._endYear = 2018;
+        job2.Display();
+
+        Resume person1 = new Resume();
+        person1._name = "Ana Clara";
+        person1._jobs.Add(job1);
+        person1._jobs.Add(job2);
+        person1.Display();
+
+        
     }
 }
