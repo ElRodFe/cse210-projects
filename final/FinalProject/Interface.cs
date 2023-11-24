392828d0a96ab427b7407fc6f7ac4b39bf2e2f57
ElRodFe <el.rod.fe@gmail.com>
2023-11-24 12:54:54 -0300

    Menu added, classes initialized

---
+++ final/FinalProject/Interface.cs
@@ -0,0 +1,18 @@
+public class Interface {
+    private string _fileName = "";
+    private List<Recipe> _listOfRecipes = new List<Recipe>();
+
+    public void Menu(string userChoice) {
+
+    }
+    public void SaveFile() {
+
+    }
+    public void LoadFile() {
+
+    }
+    public void DisplayRecipe() {
+
+    }
+    
+}
--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello FinalProject World!");
+        Interface Interface = new Interface();
+        Console.WriteLine("Welcome to the Recipe Organizer");
+        Console.WriteLine();
+        string userChoice = "";
+
+        Console.WriteLine(String.Format("{0," + Console.WindowWidth / 5 + "}", "Menu Options:"));
+        Console.WriteLine("1- New Recipe \n2- Edit Recipe \n3- Display Recipe \n4- Save File \n5- Load File \n6- Exit");
+        Console.Write("Select an option: ");
+        userChoice = Console.ReadLine();
+        Interface.Menu(userChoice);
+
+
     }
 }
